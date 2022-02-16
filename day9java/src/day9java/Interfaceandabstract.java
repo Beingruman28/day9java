63b1353e0d8bcec5f8c54b94f253528d3b96d912
ruman <mohammedruman1996@gmail.com>
2022-02-16 21:54:27 +0530

    day 9 practice.

---
+++ day9java/src/day9java/Interfaceandabstract.java
@@ -0,0 +1,30 @@
+package day9java;
+
+class C implements A{
+	public void m5()
+	{ System.out.println("hello");
+	
+	}  
+
+ abstract class B {
+	 abstract void m3();
+	 public void m4 () {
+ }
+	 
+ }
+  public interface A  {
+  
+		 int a();
+		 public void m1();
+		 public void m2();
+  } 
+public class Interfaceandabstract {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       A a = new A();
+       a.m2();
+      // a = new A();
+	} 
+}
+}
